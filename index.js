a2f30bb212b6121f026fb218a3f55d2287910291
Mateus Luiz <mateuslfreitasb@gmail.com>
2021-06-26 20:54:57 -0300

    Add some logs and new rules for the sound to play

--- index.js
+++ index.js
@@ -1,26 +1,33 @@
+require('dotenv').config()
+
 const Discord = require("discord.js")
 
-console.log("Starting Bot");
-console.log(`Token: ${process.env.BOT_TOKEN}`);
+console.log("Starting Bot")
+console.log(`Token: ${process.env.BOT_TOKEN}`)
+console.log(`For user ${process.env.USER_ID}`)
 
 const client = new Discord.Client()
-client.login(process.env.BOT_TOKEN)
 
-// install ffmpeg
 client.on('voiceStateUpdate', (oldState, newState) => {
-    // check for bot
+  console.log("User logged:")
+  console.log(newState.member.user)
 
-    if (newState.member.user.username == 'Mateus Luiz') {
-        let voiceChannel = newState.member.voice.channel
-        if(voiceChannel == null) return
+  if (newState.member.user.id == process.env.USER_ID) {
+    console.log("Issuing sound for user:")
+    console.log(newState.member.user)
 
-        voiceChannel.join(voiceChannel).then(connection =>{
-          const dispatcher = connection.play('./minion.mp3')
-          dispatcher.on("finish", end => { voiceChannel.leave() })
-        }).catch(err => console.log(err))
+    let voiceChannel = newState.member.voice.channel
+    if(voiceChannel == null) return
 
-        return
-    }
+    voiceChannel.join(voiceChannel).then(connection =>{
+      const dispatcher = connection.play('./minion.mp3')
+      dispatcher.on("finish", end => { voiceChannel.leave() })
+    }).catch(err => console.log(err))
 
     return
+  }
+
+  return
 })
+
+client.login(process.env.BOT_TOKEN)
