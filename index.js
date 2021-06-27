76ca817a1b2758a82238a6d885cf456ddf69154b
Mateus Luiz <mateuslfreitasb@gmail.com>
2021-06-26 21:19:56 -0300

    Add toggle to the bot

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express')
 const app = express()
 require('dotenv').config()
 
+let BOT_TOGGLE = false;
+
 app.listen(process.env.PORT || 3000, () => {
   const Discord = require("discord.js")
 
@@ -10,6 +12,24 @@ app.listen(process.env.PORT || 3000, () => {
   console.log(`For user ${process.env.USER_ID}`)
 
   const client = new Discord.Client()
+
+  client.on('message', message => {
+  	if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+  	const args = message.content.slice(prefix.length).trim().split(/ +/);
+  	const command = args.shift().toLowerCase();
+
+  	if (command === 'parapipa-ligar') {
+      BOT_TOGGLE = true
+  		message.channel.send('ParapipaBot ligado.');
+  	} else if (command === 'parapipa-desligar') {
+      BOT_TOGGLE = false
+  		message.channel.send('ParapipaBot desligado.');
+  	} else if (command === 'parapipa-help') {
+      message.channel.send('mande parapipa-ligar ou parapipa-desligar');
+    }
+  });
+
 
   client.on('voiceStateUpdate', (oldState, newState) => {
     console.log("User logged:")
@@ -20,7 +40,7 @@ app.listen(process.env.PORT || 3000, () => {
       console.log(newState.member.user)
 
       let voiceChannel = newState.member.voice.channel
-      if(voiceChannel == null) return
+      if(voiceChannel == null || BOT_TOGGLE == true) return
 
       voiceChannel.join(voiceChannel).then(connection =>{
         const dispatcher = connection.play('./minion.mp3')
