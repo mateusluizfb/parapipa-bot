a0a17f51ec8eddc486bed3ea301f383b9cad7fdc
Mateus Luiz <mateuslfreitasb@gmail.com>
2021-09-11 11:54:23 -0300

    Refactor dispatcher

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const dispatcher = require('./lib/dispatcher')
+const command_dispatcher = require('./lib/command_dispatcher')
 require('dotenv').config()
 
 app.listen(process.env.PORT || 3000, () => {
@@ -9,5 +9,5 @@ app.listen(process.env.PORT || 3000, () => {
   console.log(`Token: ${process.env.BOT_TOKEN}`)
   console.log(`For user ${process.env.USER_ID}`)
 
-  dispatcher();
+  command_dispatcher();
 })
