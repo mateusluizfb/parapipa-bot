185d451c0847e8f5f19c9f2162d4780bbbf78616
Mateus Luiz <mateuslfreitasb@gmail.com>
2022-01-20 20:16:18 -0300

    Refactor and update packages

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 const express = require('express')
 const app = express()
-const listener = require('./lib/listeners')
+const listeners = require('./lib/listeners')
 require('dotenv').config()
 
 app.listen(process.env.PORT || 3000, () => {
-
   console.log("Starting Bot")
   console.log(`Token: ${process.env.BOT_TOKEN}`)
 
-  listener();
+  listeners.start()
 })
